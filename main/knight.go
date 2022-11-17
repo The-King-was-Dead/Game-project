3cdfcce0e3bd475cdbea6a15bede57a438bb3c45
Romain Clemencon <romain.clemencon@mycit.ie>
2022-11-17 22:23:03 +0000

    Changed template and added working main character

---
+++ main/knight.go
@@ -0,0 +1,55 @@
+components {
+  id: "knight"
+  component: "/main/knight.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "idle"
+  type: "sprite"
+  data: "tile_set: \"/assets/knight/Idle/idle.tilesource\"\n"
+  "default_animation: \"idle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "run"
+  type: "sprite"
+  data: "tile_set: \"/assets/knight/Run/run.tilesource\"\n"
+  "default_animation: \"run\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 16.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}

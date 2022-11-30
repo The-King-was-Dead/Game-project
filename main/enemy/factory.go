958b3bc07cb43c32694cf7e0474b618a7a65729f
Eoin Fehily <eoin.fehily@mycit.ie>
2022-11-30 02:47:20 +0000

    Enemy factory added + knockback + new hit feature

---
+++ main/enemy/factory.go
@@ -0,0 +1,30 @@
+components {
+  id: "enemy_factory"
+  component: "/main/enemy/enemy_factory.factory"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "factory"
+  component: "/main/enemy/factory.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}

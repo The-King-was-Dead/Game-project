fb69800062f2c46a82c04e929d7c531b4901f253
Stefan Tcaciuc <stefan.tcaciuc.dev@gmail.com>
2022-12-18 20:18:49 +0000

    added popup menu on wizard proximity

--- main/hero/hero.go
+++ main/hero/hero.go
@@ -46,6 +46,7 @@ embedded_components {
   "mask: \"edge\"\n"
   "mask: \"enemy\"\n"
   "mask: \"light\"\n"
+  "mask: \"wizard\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_SPHERE\n"
